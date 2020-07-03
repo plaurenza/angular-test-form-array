1c469f2c2edfd3ecd48c0d9d97aa0e57e0fe9e83
plaurenza <p.laurenza@gmail.com>
2020-07-03 10:59:07 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { ControlContainer, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,15 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+
+  public profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('')
+    })
+  });
 }
